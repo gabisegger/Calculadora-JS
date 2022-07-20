001db98b0d0ac181cee569b5501c80c2b740b26f
gabisegger <gabi.segger@gmail.com>
2022-07-20 08:24:09 -0300

    feat: captures click event on operators and when selected, clears display and inserts new number

--- calculator.js
+++ calculator.js
@@ -1,10 +1,24 @@
 const display = document.getElementById('display');
 const numbers = document.querySelectorAll('[id*=key]');
+const operators = document.querySelectorAll('[id*=Operator]');
+
+let newNumber = true;
 
 const updateDisplay = (text) => {
-    display.textContent += text;
+    if(newNumber){
+        display.textContent = text;
+        newNumber = false;
+    }else{
+        display.textContent += text;
+    }    
 }
 
 const insertNumber = (e) => updateDisplay(e.target.textContent);
 
 numbers.forEach(number => number.addEventListener('click', insertNumber));
+
+const selectOperator = () => {
+    newNumber = true;
+}
+
+operators.forEach(operator => operator.addEventListener('click', selectOperator));
